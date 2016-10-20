1c6a5e5e2dfebc17327310064f75d5faff7658f5
Liu Siyuan <weifengzi2009@gmail.com>
2016-10-21 01:01:34 +0800

    modify scripts

--- sc16/md5crypt/run_with_rules.sh
+++ sc16/md5crypt/run_with_rules.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-hashcat -a 0 \
-        -m 500 \
-        -D 2 \
-        -w 2 \
-        --status \
-        --status-timer=30 \
-        -o md5_cracked.txt \
-        md5crypt.txt \
-        ../dicts/words \
-        -r ../rules/sc16.rule
+hashcat64.bin -a 0 \
+              -m 500 \
+              -D 2 \
+              -w 2 \
+              --status \
+              --status-timer=30 \
+              -o md5_cracked.txt \
+              md5crypt.txt \
+              ../dicts/words \
+              -r ../rules/sc16.rule
